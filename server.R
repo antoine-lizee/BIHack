5a8c20a03668f761fd33a4c556215fcddd1975a2
antoine.lizee <antoine.lizee@gmail.com>
2014-11-13 02:33:03 -0800

    v0.2 - still no back-end

--- server.R
+++ server.R
@@ -8,25 +8,79 @@
 library(shiny)
 library(fmsb)
 
-shinyServer(function(input, output) {
+## UTILITIES
+
+getCartCoord <- function(V) {
+  r <- V[1]
+  theta <- V[2]
+  r * c(cos(theta), sin(theta))
+}
+
+getPolarCoord <- function(V) {
+  x <- V[1]
+  y <- V[2]
+  c(r = sqrt(x^2 + y^2), theta = atan2(y,x) %% (2*pi))
+}
+
+getHex <- function(RGB) {
+  paste0("#", paste(sapply(RGB, function(i) as.character(as.hexmode(i))), collapse = ""))
+}
+
+shinyServer(function(input, output, session) {
   
+  ### DATA Unpacking
+  DS <- reactive({input$i_DS})
+  BE <- reactive({input$i_BE})
+  FE <- reactive({input$i_FE})
   data <- reactive({
     data.frame(check.names = F,
-               "Data Science" = c(5,0,input$i_DS),
-               "Back-End" = c(5,0, input$i_BE),
-               "Front-End" = c(5,0,input$i_FE))
+               "Data Science" = c(5,0,DS()),
+               "Back-End" = c(5,0, BE()),
+               "Front-End" = c(5,0, FE()))
   })
   
-  color <- reactive({
+  
+  ### PLOT
+  color.bckp <- reactive({
     topo.colors(3)[which.max(data()[3,])]
   })
-  
+  color <- reactive({
+    getHex(floor( -(c(DS(), BE(), FE()) / 5)^1.5 * 150 + 250))
+  })
   output$radarPlot <- renderPlot({
-    
     # draw radar chart
     radarchart(df = data(), axistype = 0, seg = 5, 
                pcol = "black", pfcol = color())
-    
   })
   
+  ### PROFILE
+  profile <- reactive({
+    getPolarCoord(apply(
+      apply(
+        matrix(c(DS(), BE(), FE(), 0:2 * 2*pi/3 + pi/2), nrow = 2, byrow = 3),
+        2, getCartCoord), 
+      1, sum))
+  })
+  r <- reactive({profile()[1]})
+  theta <- reactive({profile()[2]})
+  output$Profile <- renderText({
+    ifelse(r()<=1, "Balanced", 
+           ifelse(theta() < 0, "Front-End dev", 
+                  ifelse(theta() < pi/3, "Data Viz guy",
+                         ifelse(theta() < 2*pi/3, "Data Scientist",
+                                ifelse(theta() < pi, "Data dude",
+                                       ifelse(theta() < 4*pi/3, "Back-End geek", 
+                                              ifelse(theta() < 5*pi/3,  "Full Stack dev", "ERROR")))))))
+  })
+  
+  ### Updates to the UI
+  output$textInvolvement <- reactive({
+    c("A few hours", "Half of the time", "I'll be out a couple of hours", "All the time but when sleeping home")[input$i_Involvement]
+  })
+  
+  ### Debugging
+  output$DEBUG <- renderPrint({
+    print(profile())
+    print(color())
+  })
 })
--- ui.R
+++ ui.R
@@ -9,60 +9,89 @@ listOfNames <- as.list(c("blabla"="321", "antoine.lizee"="684"))
 listOfSkills <- list(c("R", "Python", "C++", "C", "go", "java", "Hadoop", "sysops", "Node.js", "PHP", "Frameworks", "Django", "SQL", "noSQL", "ruby", "ROR"),
                      c("js", "jQuery", "CSS", "D3S", "Shiny", "Objective-C", "Android"),
                      c("R", "Python", "Julia", "ML", "DataVis", "Algorithmics", "Stats"))
+listOfDatasets <- list("Education" = c("01 - Help Teachers Get What They Need", 
+                                       "04 - Help Donors Choose Better Serve Teachers"),
+                       "Safety & Crime" = c("02 - Fight Child Sexual Exploitation", 
+                                            "05 - Help Police Prevent Domestic Violence",
+                                            "13 - Visualize 911 Service Calls & Police Reports"),
+                       "Environment" = c("06 - Parks and Rec",
+                                         "09 - Predicting Climate Threats"),
+                       "Healthcare" = c("08 - Prepare California for Elderly Health Needs",
+                                        "07 - Ebola – Mining Available Data"),
+                       "Industry" =c("03 - Reduce Accidents in Mines",
+                                     "10 - Assess Flood Risk of Roads and Airports",
+                                     "11 - Prevent Supertankers from Grounding",
+                                     "12 - Predict Whereabouts of Types of Fish"
+                       )
+)
 
 library(shiny)
 
 shinyUI(navbarPage(
   "BayesImpact Hackathon Profiler",
   tabPanel("Your Profile",
-           fluidPage(fluidRow(
-             column(3, 
-                    
-                    # Application title
-                    titlePanel("Define your skills"),
-                    hr(),
-                    wellPanel(
-                      h3("Login Info", align = "center"),
-                      helpText("Create a login or select one that you have already created."),
-                      selectizeInput(inputId = "Login", label = NULL, choices = c(list("Login" = ""), listOfNames), multiple = FALSE, options = list(create = "true")),
-                      helpText("Type the associated password (or create one). Use a Weak, non-important password, like the four random digits suggested below."),
-                      textInput(inputId = "Password", label = NULL, value = paste0(sample(9,4), collapse = "")) ,
+           fluidPage(
+             fluidRow(
+               column(3, 
                       
-                      fluidRow(actionButton("b_Create", "Create Profile")),
-                      fluidRow(actionButton("b_Load", "Load Profile")), 
-                      fluidRow(actionButton("b_Update", "Update Profile"))
-                    )),
-             column(9,
-                    fluidRow( 
-                      column(4, 
-                             wellPanel(
-                               h4("Back-end"),
-                               sliderInput("i_BE", label = "Overall Skill Level", min = 0, max = 5, step = 0.5, value = 0, ticks = TRUE),
-                               selectInput("c_BE", label = "Select / add some skills", choices = c("keyword..."="", listOfSkills[[1]]), multiple = TRUE, selectize = TRUE, options("create" = "yes"))
-                             )),
-                      column(4, 
-                             wellPanel(
-                               h4("Front-end"),
-                               sliderInput("i_FE", label = "Overall Skill Level", min = 0, max = 5, step = 0.5, value = 0, ticks = TRUE),
-                               selectInput("c_FE", label = "Select / add some skills", choices = c("keyword..."="", listOfSkills[[2]]), multiple = TRUE, selectize = TRUE, options("create" = "yes"))
-                             )),
-                      column(4, 
-                             wellPanel(
-                               h4("Data-Science"),
-                               sliderInput("i_DS", label = "Overall Skill Level", min = 0, max = 5, step = 0.5, value = 0, ticks = TRUE),
-                               selectInput("c_DS", label = "Select / add some skills", choices = c("keyword..."="", listOfSkills[[3]]), multiple = TRUE, selectize = TRUE, options("create" = "yes"))
-                             ))
-                    ),
-                    hr(),
-                    plotOutput("radarPlot")
+                      # Application title
+                      titlePanel("Define your skills"),
+                      hr(),
+                      wellPanel(
+                        h2("Login Info", align = "center"),
+                        helpText("Create a login or select one that you have already created."),
+                        selectizeInput(inputId = "Login", label = NULL, choices = c(list("Login" = ""), listOfNames), multiple = FALSE, options = list(create = "true")),
+                        helpText("Type the associated password (or create one). Use a Weak, non-important password, like the four random digits suggested below."),
+                        textInput(inputId = "Password", label = NULL, value = paste0(sample(9,4), collapse = "")) ,
+                        
+                        fluidRow(actionButton("b_Create", "Create Profile")),
+                        fluidRow(actionButton("b_Load", "Load Profile")), 
+                        fluidRow(actionButton("b_Update", "Update Profile"))
+                      )),
+               column(9,
+                      fluidRow( 
+                        column(4, 
+                               wellPanel(
+                                 h4("Back-end"),
+                                 sliderInput("i_BE", label = "Estimate your overall skill Level", min = 0, max = 5, step = 0.5, value = 0, ticks = TRUE),
+                                 selectInput("c_BE", label = "Select / add some skills", choices = c("keyword..."="", listOfSkills[[1]]), multiple = TRUE, selectize = TRUE, options("create" = "yes"))
+                               )),
+                        column(4, 
+                               wellPanel(
+                                 h4("Front-end"),
+                                 sliderInput("i_FE", label = "Estimate your overall skill Level", min = 0, max = 5, step = 0.5, value = 0, ticks = TRUE),
+                                 selectInput("c_FE", label = "Select / add some skills", choices = c("keyword..."="", listOfSkills[[2]]), multiple = TRUE, selectize = TRUE, options("create" = "yes"))
+                               )),
+                        column(4, 
+                               wellPanel(
+                                 h4("Data-Science"),
+                                 sliderInput("i_DS", label = "Estimate your overall skill Level", min = 0, max = 5, step = 0.5, value = 0, ticks = TRUE),
+                                 selectInput("c_DS", label = "Select / add some skills", choices = c("keyword..."="", listOfSkills[[3]]), multiple = TRUE, selectize = TRUE, options("create" = "yes"))
+                               ))
+                      ),
+                      hr(),
+                      fluidRow(
+                        column(8,
+                               plotOutput("radarPlot")),
+                        column(4,
+                               p("Your Profie is:", align = "center"),
+                               h2(textOutput("Profile", inline = TRUE), align = "center"),
+                               wellPanel(
+                                 selectInput(inputId = "s_Datasets", label = "Select the dataset(s) your are interesting to work on:", choices = listOfDatasets, multiple = TRUE),
+                                 sliderInput("i_Involvement", label = "How much time do you intend to spend at the hackathon?", min = 1, max = 4, value = 2, step = 1 )
+                               )
+                        )
+                      )
+               )
              )
-           )
            )
   ),
   tabPanel("All Profiles",
            h4("The database with all profiles is available", a(href = "www",  "here"))
-    ),
+  ),
   tabPanel("Potential Teams"
-           )
+  ),
+  tabPanel("Debug",
+           verbatimTextOutput("DEBUG"))
 )
 )
