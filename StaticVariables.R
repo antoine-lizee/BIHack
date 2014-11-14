9041dc2d7ad8e26d18107707d8aacd1a34698865
antoine.lizee <antoine.lizee@gmail.com>
2014-11-13 19:29:59 -0800

    fixed

---
+++ StaticVariables.R
@@ -0,0 +1,18 @@
+listOfSkills <- list(c("R", "Python", "C++", "C", "go", "java", "Hadoop", "sysops", "Node.js", "PHP", "Frameworks", "Django", "SQL", "noSQL", "ruby", "ROR"),
+                     c("js", "jQuery", "CSS", "D3S", "Shiny", "Objective-C", "Android"),
+                     c("R", "Python", "Julia", "ML", "DataVis", "Algorithmics", "Stats"))
+listOfDatasets <- list("Education" = c("01 - Help Teachers Get What They Need", 
+                                       "04 - Help Donors Choose Better Serve Teachers"),
+                       "Safety & Crime" = c("02 - Fight Child Sexual Exploitation", 
+                                            "05 - Help Police Prevent Domestic Violence",
+                                            "13 - Visualize 911 Service Calls & Police Reports"),
+                       "Environment" = c("06 - Parks and Rec",
+                                         "09 - Predicting Climate Threats"),
+                       "Healthcare" = c("08 - Prepare California for Elderly Health Needs",
+                                        "07 - Ebola – Mining Available Data"),
+                       "Industry" =c("03 - Reduce Accidents in Mines",
+                                     "10 - Assess Flood Risk of Roads and Airports",
+                                     "11 - Prevent Supertankers from Grounding",
+                                     "12 - Predict Whereabouts of Types of Fish"
+                       )
+)
--- server.R
+++ server.R
@@ -1,94 +1,99 @@
 
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
 
 library(shiny)
-# options(shiny.trace=FALSE)
-# options(shiny.error=NULL)
-# library(fmsb)
-# library(RSQLite)
-# # library(jsonlite)
-# library(rjson)
+options(shiny.trace=FALSE)
+options(shiny.error=traceback)
+library(fmsb)
+library(RSQLite)
+# library(jsonlite)
+library(rjson)
 
 source("StaticVariables.R")
 
 ## UTILITIES
-# 
-# getCartCoord <- function(V) {
-#   r <- V[1]
-#   theta <- V[2]
-#   r * c(cos(theta), sin(theta))
-# }
-# 
-# getPolarCoord <- function(V) {
-#   x <- V[1]
-#   y <- V[2]
-#   c(r = sqrt(x^2 + y^2), theta = atan2(y,x) %% (2*pi))
-# }
-# 
-# getHex <- function(RGB) {
-#   paste0("#", paste(sapply(RGB, function(i) as.character(as.hexmode(i))), collapse = ""))
-# }
-# 
-# ## BACKEND - Database
-# # Schema:
-# user0 <- data.frame(Name = "USER0", Password = "765",
-#                     DS = 0, BE = 3, FE = 2, 
-#                     Datasets = toJSON(c(listOfDatasets$Education[1], listOfDatasets$Education[2])),
-#                     Involvement = 2,
-#                     stringsAsFactors = F)
-# # Initialization
-# userDBName <- "BIH_users.sqlite"
-# userTableName <- "BIH_users"
-# con <- RSQLite::dbConnect(SQLite(), userDBName, cache_size = 5000, synchronous = "full")
-# if (!dbExistsTable(con, userTableName)) {
-#   dbWriteTable(con, name = userTableName, value = user0[0,], row.names = F)
-#   #   dbGetQuery(con, paste("DELETE FROM", userTableName, "WHERE Name = 'USER0'"))
-# }
-# # # Reset code:
-# # dbDisconnect(con)
-# # file.remove(userDBName)
-# 
-# getUser <- function(Name) {
-#   user <- dbGetQuery(con, paste0("SELECT * FROM ", userTableName, " WHERE Name = '", Name, "'"))
-#   if (nrow(user) == 0) {
-#     return(NULL)
-#   } else {
-#     return(user)
-#   }
-# }
-# 
-# unpackDF <- function(User) {
-#   stopifnot(nrow(User) == 1)
-#   modDf <- sapply(User, 
-#                   function(col) { 
-#                     if(is.character(col)) 
-#                       paste0("'", col, "'")
-#                     else
-#                       col
-#                   })
-#   return(paste0("( ", paste(modDf, collapse = ", "), ")"))
-# }
-# 
-# createUser <- function(User) {
-#   if (User$Name == "") {
-#     warning("No name provided for the user")
-#     return(FALSE)
-#   }
-#   dbSendQuery(con, paste0("INSERT INTO ", userTableName, " VALUES ", unpackDF(User)))
-# }
-# 
-# updateUser <- function(User) {
-#   if (User$Name == "") {
-#     warning("No name provided for the user")
-#     return(FALSE)
-#   }
-#   ## UGLY hack
-#   dbGetQuery(con, paste("DELETE FROM", userTableName, "WHERE Name = '", User$Name,"'"))
-#   dbSendQuery(con, paste0("INSERT INTO ", userTableName, " VALUES ", unpackDF(User)))
-# }
-# 
-# getListOfUsers <- function() {
-#   sqliteQuickColumn(con, userTableName, "Name")
-# }
+
+getCartCoord <- function(V) {
+  r <- V[1]
+  theta <- V[2]
+  r * c(cos(theta), sin(theta))
+}
+
+getPolarCoord <- function(V) {
+  x <- V[1]
+  y <- V[2]
+  c(r = sqrt(x^2 + y^2), theta = atan2(y,x) %% (2*pi))
+}
+
+getHex <- function(RGB) {
+  paste0("#", paste(sapply(RGB, function(i) as.character(as.hexmode(i))), collapse = ""))
+}
+
+## BACKEND - Database
+# Schema:
+user0 <- data.frame(Name = "USER0", Password = "765",
+                    DS = 0, BE = 3, FE = 2, 
+                    Datasets = toJSON(c(listOfDatasets$Education[1], listOfDatasets$Education[2])),
+                    Involvement = 2,
+                    stringsAsFactors = F)
+# Initialization
+userDBName <- "BIH_users.sqlite"
+userTableName <- "BIH_users"
+con <- RSQLite::dbConnect(SQLite(), userDBName, cache_size = 5000, synchronous = "full")
+if (!dbExistsTable(con, userTableName)) {
+  dbWriteTable(con, name = userTableName, value = user0[0,], row.names = F)
+  #   dbGetQuery(con, paste("DELETE FROM", userTableName, "WHERE Name = 'USER0'"))
+}
+# # Reset code:
+# dbDisconnect(con)
+# file.remove(userDBName)
+
+getUser <- function(Name) {
+  user <- dbGetQuery(con, paste0("SELECT * FROM ", userTableName, " WHERE Name = '", Name, "'"))
+  if (nrow(user) == 0) {
+    return(NULL)
+  } else {
+    return(user)
+  }
+}
+
+unpackDF <- function(User) {
+  stopifnot(nrow(User) == 1)
+  modDf <- sapply(User, 
+                  function(col) { 
+                    if(is.character(col)) 
+                      paste0("'", col, "'")
+                    else
+                      col
+                  })
+  return(paste0("( ", paste(modDf, collapse = ", "), ")"))
+}
+
+createUser <- function(User) {
+  if (User$Name == "") {
+    warning("No name provided for the user")
+    return(FALSE)
+  }
+  dbSendQuery(con, paste0("INSERT INTO ", userTableName, " VALUES ", unpackDF(User)))
+}
+
+updateUser <- function(User) {
+  if (User$Name == "") {
+    warning("No name provided for the user")
+    return(FALSE)
+  }
+  ## UGLY hack
+  dbGetQuery(con, paste("DELETE FROM", userTableName, "WHERE Name = '", User$Name,"'"))
+  dbSendQuery(con, paste0("INSERT INTO ", userTableName, " VALUES ", unpackDF(User)))
+}
+
+getListOfUsers <- function() {
+  sqliteQuickColumn(con, userTableName, "Name")
+}
 
 
 shinyServer(function(input, output, session) {
@@ -104,97 +109,96 @@ shinyServer(function(input, output, session) {
                "Front-End" = c(5,0, FE()))
   })
   
-  #   User <- reactive({
-  #     user <- user0
-  #     user$Name <- input$s_Name
-  #     user$Password <- input$s_Password
-  #     user$DS <- DS()
-  #     user$BE <- BE()
-  #     user$FE <- FE()
-  #     user$Datasets <- toJSON(input$s_Datasets)
-  #     user$Involvement <- input$i_Involvement
-  #     return(user)
-  #   })
+  User <- reactive({
+    user <- user0
+    user$Name <- input$s_Name
+    user$Password <- input$s_Password
+    user$DS <- DS()
+    user$BE <- BE()
+    user$FE <- FE()
+    user$Datasets <- toJSON(input$s_Datasets)
+    user$Involvement <- input$i_Involvement
+    return(user)
+  })
+  
+  ### PLOT
+  color.bckp <- reactive({
+    topo.colors(3)[which.max(data()[3,])]
+  })
+  color <- reactive({
+    getHex(floor( -(c(DS(), BE(), FE()) / 5)^1.5 * 150 + 250))
+  })
+  output$radarPlot <- renderPlot({
+    # draw radar chart
+    radarchart(df = data(), axistype = 0, seg = 5, 
+               pcol = "black", pfcol = color())
+  })
+  
+  ### PROFILE
+  profile <- reactive({
+    getPolarCoord(apply(
+      apply(
+        matrix(c(DS(), BE(), FE(), 0:2 * 2*pi/3 + pi/2), nrow = 2, byrow = 3),
+        2, getCartCoord), 
+      1, sum))
+  })
+  r <- reactive({profile()[1]})
+  theta <- reactive({profile()[2]})
+  output$Profile <- renderText({
+    ifelse((r()/max(data()[3,]))<=1/3, "Balanced", 
+           ifelse(theta() < 0, "ERROR", 
+                  ifelse(theta() < pi/3, "Data Viz guy",
+                         ifelse(theta() < 2*pi/3, "Data Scientist",
+                                ifelse(theta() < pi, "Data dude",
+                                       ifelse(theta() < 4*pi/3, "Back-End geek", 
+                                              ifelse(theta() < 5*pi/3,  "Full Stack dev", "Front-End dev")))))))
+  })
+  
+  ### Updates to the UI
   #   
-  #   ### PLOT
-  #   color.bckp <- reactive({
-  #     topo.colors(3)[which.max(data()[3,])]
+  #   output$LoginField <- renderUI({
+  #     selectizeInput(inputId = "s_Name", label = NULL, choices = c(c("Login" = "", getListOfUsers()), multiple = FALSE, options = list(create = "true"))
   #   })
-  #   color <- reactive({
-  #     getHex(floor( -(c(DS(), BE(), FE()) / 5)^1.5 * 150 + 250))
-  #   })
-  #   output$radarPlot <- renderPlot({
-  #     # draw radar chart
-  #     radarchart(df = data(), axistype = 0, seg = 5, 
-  #                pcol = "black", pfcol = color())
-  #   })
-  #   
-  #   ### PROFILE
-  #   profile <- reactive({
-  #     getPolarCoord(apply(
-  #       apply(
-  #         matrix(c(DS(), BE(), FE(), 0:2 * 2*pi/3 + pi/2), nrow = 2, byrow = 3),
-  #         2, getCartCoord), 
-  #       1, sum))
-  #   })
-  #   r <- reactive({profile()[1]})
-  #   theta <- reactive({profile()[2]})
-  #   output$Profile <- renderText({
-  #     ifelse((r()/max(data()[3,]))<=1/3, "Balanced", 
-  #            ifelse(theta() < 0, "ERROR", 
-  #                   ifelse(theta() < pi/3, "Data Viz guy",
-  #                          ifelse(theta() < 2*pi/3, "Data Scientist",
-  #                                 ifelse(theta() < pi, "Data dude",
-  #                                        ifelse(theta() < 4*pi/3, "Back-End geek", 
-  #                                               ifelse(theta() < 5*pi/3,  "Full Stack dev", "Front-End dev")))))))
-  #   })
-  #   
-  #   ### Updates to the UI
-  #   #   
-  #   #   output$LoginField <- renderUI({
-  #   #     selectizeInput(inputId = "s_Name", label = NULL, choices = c(c("Login" = "", getListOfUsers()), multiple = FALSE, options = list(create = "true"))
-  #   #   })
-  #   
-  #   output$textInvolvement <- reactive({
-  #     c("A few hours", "Half of the time", "I'll be out a couple of hours", "All the time but when sleeping home")[input$i_Involvement]
-  #   })
-  #   
-  #   ### Backend Listeners
-  #   
-  #   output$loginErrorCreate <- renderText({
-  #     if (input$b_Create != 0){ # Try to create the record
-  #       if (isolate(User()['Name']) == "") {
-  #         return("ERROR: No pseudo provided.")
-  #       }
-  #       ## Try to retrieve the user data in a non-dependent fashion
-  #       user <- isolate(getUser(User()['Name']))
-  #       if (!is.null(user)) {
-  #         return("ERROR: The user already exists... Try another one or load the existing user by also typing the password.")
-  #       } else {
-  #         createUser(isolate(User()))
-  #       }
-  #     }
-  #   })
-  #   
-  #   output$loginErrorLoad <- renderText({
-  #     if (input$b_Load != 0){ # Try to create the record
-  #       ## Retrieve the user data in a non-dependent fashion
-  #       user <- isolate(getUser(User()['Name']))
-  #       if (is.null(user)) {
-  #         "ERROR: The user with this pseudo doesn't exist yet."
-  #       } else {
-  #         if (user$Password != isolate(User()['Password']))
-  #           "ERROR: The password doesn't match the records..."
-  #         else {
-  #           "User Loaded"
-  #         }
-  #       }
-  #     }
-  #   })
+  
+  output$textInvolvement <- reactive({
+    c("A few hours", "Half of the time", "I'll be out a couple of hours", "All the time but when sleeping home")[input$i_Involvement]
+  })
+  
+  ### Backend Listeners
+  
+  output$loginErrorCreate <- renderText({
+    if (input$b_Create != 0){ # Try to create the record
+      if (isolate(User()['Name']) == "") {
+        return("ERROR: No pseudo provided.")
+      }
+      ## Try to retrieve the user data in a non-dependent fashion
+      user <- isolate(getUser(User()['Name']))
+      if (!is.null(user)) {
+        return("ERROR: The user already exists... Try another one or load the existing user by also typing the password.")
+      } else {
+        createUser(isolate(User()))
+      }
+    }
+  })
+  
+  output$loginErrorLoad <- renderText({
+    if (input$b_Load != 0){ # Try to create the record
+      ## Retrieve the user data in a non-dependent fashion
+      user <- isolate(getUser(User()['Name']))
+      if (is.null(user)) {
+        "ERROR: The user with this pseudo doesn't exist yet."
+      } else {
+        if (user$Password != isolate(User()['Password']))
+          "ERROR: The password doesn't match the records..."
+        else {
+          "User Loaded"
+        }
+      }
+    }
+  })
   
   ### Debugging
   output$DEBUG <- renderPrint({
-    print(data())
     print(profile())
     print(color())
     print(User())
